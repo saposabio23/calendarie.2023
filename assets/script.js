07011bdc7807f826c971567c7c64bb5a2f6c74f6
Pablo Moreno <ehtomer@gmail.com>
2023-04-24 16:43:08 +0200

    test form?

--- assets/script.js
+++ assets/script.js
@@ -149,4 +149,11 @@ cumpleanos.addEventListener('change', () => {
     }
 });
 
+function disableEmptyInputs(form) {
+    var controls = form.elements;
+    for (var i=0, iLen=controls.length; i<iLen; i++) {
+      controls[i].disabled = controls[i].value == '';
+    }
+  }
+
 
